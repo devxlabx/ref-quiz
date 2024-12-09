d7b2b7efb5ff74302fc79f09004a845455c992cb
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-12-09 16:40:27 +0100

    feat: add new helpers to check email, names and passwords

--- refquiz-front/src/utils/helpers.ts
+++ refquiz-front/src/utils/helpers.ts
@@ -52,3 +52,38 @@ export const shuffleArray = <T>(array: T[]): T[] => {
 
   return shuffledArray
 }
+export const validateNameInput = (name: string): boolean => {
+  const cleanedName = name?.trim();
+  const nameRegex = /^[a-zA-Z]{2,50}$/;
+  return nameRegex.test(cleanedName || "");
+};
+
+export const validateEmailInput = (email: string): boolean => {
+    const cleanedEmail = email?.trim(); 
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(cleanedEmail || "");
+};
+
+export const validatePasswordInput = (password: string): boolean => {
+  if (!password || password.length === 0) {
+    return false; 
+  }
+    // Cette regex exige :
+    // - Au moins une lettre minuscule (`?=.*[a-z]`)
+    // - Au moins une lettre majuscule (`?=.*[A-Z]`)
+    // - Au moins un chiffre (`?=.*\d`)
+    // - Au moins un caractère spécial(y compris l'espace) (`?=.*[!@#$%^&*()_+]`)
+    // - Minimum 8 caractères
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+ .])[a-zA-Z\d!@#$%^&*()_+ .]{8,}$/;
+
+  return passwordRegex.test(password);
+};
+    
+
+export const validateMatchPasswordInput = (passwordToValidate : string, password: string): boolean => {
+  return password === passwordToValidate;
+};
+
+export const checkCredentials = (emailInput : string, emailUser: string, passwordInput: string, passwordUser: string): boolean => {
+  return passwordInput === passwordUser && emailInput === emailUser;
+};
