c5a98a0c7bb727c21b8da3a5d10a132f560601a9
MIDANI <32643487+MIDANI-Bassem@users.noreply.github.com>
2024-12-09 16:38:04 +0100

    feat: adding the register screen component

---
+++ refquiz-front/src/components/RegisterScreen/index.tsx
@@ -0,0 +1,218 @@
+import Button from '../ui/Button'
+import styled from 'styled-components'
+import { AppLogo } from '../../config/icons'
+import React, { useState } from 'react'
+
+import {
+    CenterCardContainer,
+    LogoContainer,
+    PageCenter,
+} from '../../styles/Global'
+import { useQuiz } from '../../context/QuizContext'
+import { ScreenTypes } from '../../types'
+import { validateEmailInput, validateMatchPasswordInput, validateNameInput, validatePasswordInput } from '../../utils/helpers'
+
+const DetailText = styled.p`
+    font-weight: 500;
+    font-size: 25px;
+    line-height: 29px;
+`
+
+const Heading = styled.h2`
+    font-size: 32px;
+    font-weight: 700;
+    margin-bottom: 20px;
+    text-align: center;
+`
+
+const Input = styled.input`
+    font-size: 20px;
+    font-weight: 100;
+    height: 2.5rem;
+    margin-top: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+`
+
+const ErrorMessage = styled.p`
+    color:  #E63946;
+    font-size: 12px;
+    margin-top: 5px;
+`
+
+const Div = styled.div`
+    width: 75%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 10px;
+`
+
+const DivForm = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 75%;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    position: relative;
+`
+
+function RegisterScreen() {
+    const { setCurrentScreen } = useQuiz()
+
+    const [email, setEmail] = useState<string>("")
+    const [firstName, setFirstName] = useState<string>("")
+    const [lastName, setLastName] = useState<string>("")
+    const [matchedPassword, setMatchedPassword] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [errors, setErrors] = useState<{ [key: string]: string }>({}) 
+    const user = {
+        "email" : "",
+        "password" : "",
+    }
+    let validMail = false;
+    let validFirstName = false;
+    let validLastName = false;
+    let validPassword = false;
+    let validPasswordMatch = false;
+
+    const firstNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if (validateNameInput(value)) {
+            setFirstName(value);
+            validFirstName = true
+            setErrors((prevErrors) => ({ ...prevErrors, firstName: "" })); 
+        } else {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                firstName: "Le nom ne doit pas contenir de caractères spéciaux et doit comporter au moins 2 à 50 caractères.",
+            }));
+        }
+    }
+   
+
+    const lastNameHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if (validateNameInput(value)) {
+            setLastName(value);
+            validLastName = true;
+            setErrors((prevErrors) => ({ ...prevErrors, lastName: "" })); 
+        } else {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                lastName: "Le prénom ne doit pas contenir de caractères spéciaux et doit comporter au moins 2 à 50 caractères.",
+            }));
+        }
+    }
+    
+
+    const emailHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if (validateEmailInput(value)) {
+            setEmail(value)
+            validMail = true;
+            setErrors((prevErrors) => ({ ...prevErrors, email: "" })); 
+        } else {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                email: "L'email doit être valide et suivre le format 'exemple@domaine.com'.",
+            }));
+        }
+        
+    }
+    
+
+    const passwordHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.target.value;
+        if (validatePasswordInput(value)) {
+            setPassword(value)
+            validPassword = true
+            setErrors((prevErrors) => ({ ...prevErrors, password: "" })); 
+        } else {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                password: "Le mot de passe doit contenir au moins 8 caractères, dont une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial.",
+            }));
+        }
+    }
+    
+
+    const passwordMatchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setMatchedPassword(e.target.value)
+        const value = e.target.value;
+        if (validateMatchPasswordInput(value,password)) {
+            setMatchedPassword(value)
+            validPasswordMatch = true;
+            setErrors((prevErrors) => ({ ...prevErrors, passwordMatch: "" })); 
+        } else {
+            setErrors((prevErrors) => ({
+                ...prevErrors,
+                passwordMatch: "Les mots de passe doivent correspondre.",
+            }));
+        }
+    }
+    
+
+    const goToLoginScreen = () => {
+        setCurrentScreen(ScreenTypes.LoginScreen)
+    }
+    const submitData = () => {
+        if(validFirstName && validLastName && validMail && validPassword && validPasswordMatch){
+            user.email = email;
+            user.password = password;
+            setCurrentScreen(ScreenTypes.LoginScreen)
+        }
+    }
+
+    return (
+        <PageCenter justifyCenter hasBackgroundImage>
+            <CenterCardContainer>
+                <LogoContainer>
+                    <AppLogo />
+                </LogoContainer>
+                <Heading>
+                    Création du compte
+                </Heading>
+
+                <DivForm id="first-name-container">
+                    <DetailText>Nom :</DetailText>
+                    <Input type="text" placeholder='Doe' onChange={firstNameHandler} required />
+                    {errors.firstName && <ErrorMessage>{errors.firstName}</ErrorMessage>} 
+                </DivForm>
+
+                <DivForm id="last-name-container">
+                    <DetailText>Prénom :</DetailText>
+                    <Input type="text" placeholder='John' onChange={lastNameHandler} required />
+                    {errors.lastName && <ErrorMessage>{errors.lastName}</ErrorMessage>} 
+                </DivForm>
+
+                <DivForm id="email-container">
+                    <DetailText>Email :</DetailText>
+                    <Input type="email" placeholder='john.doe@mail.com' onChange={emailHandler} required />
+                    {errors.email && <ErrorMessage>{errors.email}</ErrorMessage>} 
+                </DivForm>
+
+                <DivForm id="password-container">
+                    <DetailText>Mot de passe :</DetailText>
+                    <Input type="password" placeholder='***********' onChange={passwordHandler} required />
+                    {errors.password && <ErrorMessage>{errors.password}</ErrorMessage>} 
+                </DivForm>
+
+                <DivForm id="password-tomatch-container">
+                    <DetailText>Confirmer le mot de passe :</DetailText>
+                    <Input type="password" placeholder='***********' onChange={passwordMatchHandler} required />
+                    {errors.passwordMatch && <ErrorMessage>{errors.passwordMatch}</ErrorMessage>} 
+                </DivForm>
+
+                <Div>
+                    <Button text="Retour" onClick={goToLoginScreen} bold />
+                    <Button text="Créer" onClick={submitData} bold />
+                </Div>
+                
+            </CenterCardContainer>
+        </PageCenter>
+    )
+}
+
+export default RegisterScreen
