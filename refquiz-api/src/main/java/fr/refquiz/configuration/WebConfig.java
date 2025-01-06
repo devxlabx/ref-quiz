945bc126d9a6f3224f09bb1cc71ff6d52985a7f4
MIDANI-Bassem <32643487+MIDANI-Bassem@users.noreply.github.com>
2025-01-06 14:52:00 +0100

    feat: register a user (#8)
    * feat: Adding a new user
    
    * Update pom.xml
    
    * refactor: deleting openapi documentation
    
    * refactor: deleting vscode folder

--- refquiz-api/src/main/java/fr/refquiz/configuration/WebConfig.java
+++ refquiz-api/src/main/java/fr/refquiz/configuration/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
@@ -11,8 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
+
 }
