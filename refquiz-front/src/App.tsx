727b865ca3b406075f28f828cc4a8aed19337b39
Yassim <nador44@gmail.com>
2025-01-09 22:28:54 +0100

    feat: add routing

--- refquiz-front/src/App.tsx
+++ refquiz-front/src/App.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import Cookies from 'js-cookie'
 
 import Main from './features/Main'
 import ToggleTheme from './components/ToggleTheme'
@@ -7,13 +9,22 @@ import QuizProvider from './context/QuizProvider'
 import './index.css'
 import { GlobalStyles } from './styles/Global'
 import { themes } from './styles/Theme'
-
+import LoginPage from "./pages/LoginPage";
+import QuizTopicsScreen from "./features/QuizTopicsScreen";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   const [currentTheme, setCurrentTheme] = useState(() => {
     const savedTheme = localStorage.getItem('theme')
     return savedTheme || 'light'
   })
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  useEffect(() => {
+    const authToken = Cookies.get('authToken')
+    setIsAuthenticated(!!authToken)
+  }, [])
 
   const toggleTheme = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { checked } = e.target
@@ -24,19 +35,48 @@ function App() {
   const theme = currentTheme === 'light' ? themes.light : themes.dark
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <QuizProvider>
-        <ToggleTheme
-          onChange={toggleTheme}
-          currentTheme={currentTheme}
-          checked={currentTheme === 'dark'}
-          id="toggleTheme"
-          value="theme"
-        />
-        <Main />
-      </QuizProvider>
-    </ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <QuizProvider>
+          <Router>
+            <ToggleTheme
+                onChange={toggleTheme}
+                currentTheme={currentTheme}
+                checked={currentTheme === 'dark'}
+                id="toggleTheme"
+                value="theme"
+            />
+            <Routes>
+              {/* Route principale avec redirection basée sur l'état d'authentification */}
+              <Route
+                  path="/"
+                  element={
+                    isAuthenticated ? (
+                        <Navigate to="/main" replace />
+                    ) : (
+                        <Navigate to="/login" replace />
+                    )
+                  }
+              />
+
+              {/* Route vers la page de connexion */}
+              <Route path="/login" element={<LoginPage />} />
+                {/* Route vers la page de création d'un compte */}
+               <Route path="/register" element={<RegisterPage />} />
+
+              {/* Route principale après connexion */}
+              <Route path="/main" element={<Main />} />
+
+              {/* Route vers l'écran des sujets de quiz */}
+              <Route path="/topics" element={<QuizTopicsScreen />} />
+
+              {/* Route par défaut pour les pages non trouvées */}
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+
+          </Router>
+        </QuizProvider>
+      </ThemeProvider>
   )
 }
 
--- refquiz-front/src/features/Login/Login.tsx
+++ refquiz-front/src/features/Login/Login.tsx
@@ -1,4 +1,3 @@
-import Button from '../../components/Button/Button'
 import React, { useState } from 'react'
 import { useQuiz } from '../../context/QuizContext'
 import { ScreenTypes } from '../../types'
@@ -6,88 +5,86 @@ import { validateEmailInput } from '../../utils/helpers'
 import InputField from '../../components/Input/Input'
 import Card from '../../components/Card/Card'
 import Error from '../../components/Errors/Error'
+import Button from '../../components/Button/Button'
+import { useNavigate } from 'react-router-dom' // Import useNavigate
 import './Login.css';
 
 function Login() {
-    const { setCurrentScreen } = useQuiz()
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const [errors, setErrors] = useState<{ [key: string]: string }>({})
+    const { setCurrentScreen } = useQuiz();
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [errors, setErrors] = useState<{ [key: string]: string }>({});
+    const navigate = useNavigate(); // Instanciation du navigate
 
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value
+        const value = e.target.value;
         if (validateEmailInput(value)) {
-            setEmail(value)
-            setErrors((prevErrors) => ({ ...prevErrors, email: "" }))
+            setEmail(value);
+            setErrors((prevErrors) => ({ ...prevErrors, email: '' }));
         } else {
             setErrors((prevErrors) => ({
                 ...prevErrors,
                 email: "L'email doit être valide et suivre le format 'exemple@domaine.com'.",
-            }))
+            }));
         }
-    }
+    };
 
     const removePlaceHolder = (e: React.MouseEvent<HTMLInputElement>) => {
-        e.currentTarget.placeholder = ""
-        e.currentTarget.className += " border"
-
-    }
+        e.currentTarget.placeholder = '';
+        e.currentTarget.className += ' border';
+    };
 
     const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value)
-    }
+        setPassword(e.target.value);
+    };
 
     const goToQuizTopicsScreen = (e: React.FormEvent) => {
-        e.preventDefault()
-        setCurrentScreen(ScreenTypes.QuizTopicsScreen)
-    }
+        e.preventDefault();
+        navigate('/topics'); // Utilisation du navigate pour rediriger vers /register
+    };
 
     const goToRegisterScreen = () => {
-        setCurrentScreen(ScreenTypes.RegisterPage)
-    }
+        navigate('/register'); // Utilisation du navigate pour rediriger vers /register
+    };
 
     return (
-            <Card className="login-card ">
-                <InputField
-                    label="E-mail"
-                    type="email"
-                    placeholder="john.doe@mail.com"
-                    onChange={handleEmailChange}
-                    onClick={removePlaceHolder}
-                />
-                <Error>
-                    {errors.email && errors.email}
-                </Error>
-        
-                <InputField
-                    label="Mot de Passe"
-                    type="password"
-                    placeholder="***********"
-                    value={password}
-                    onChange={handlePasswordChange}
-                    onClick={removePlaceHolder}
-                />
-                <Error>
-                </Error>
-        
-                <div className="button-container">
-                    <Button onClick={goToQuizTopicsScreen}>CONNEXION</Button>
-                </div>
-  
+        <Card className="login-card ">
+            <InputField
+                label="E-mail"
+                type="email"
+                placeholder="john.doe@mail.com"
+                onChange={handleEmailChange}
+                onClick={removePlaceHolder}
+            />
+            <Error>{errors.email && errors.email}</Error>
 
-                <div className="link-container">
-                    <a href="#" className="text-white-400 hover:text-gray-300 transition">
-                        Mot de passe oublié ?
-                    </a>
-                    <a
-                        onClick={goToRegisterScreen}
-                        className="text-white-400 cursor-pointer hover:text-gray-300 transition"
-                    >
-                        Créer un compte
-                    </a>
-                </div>
-            </Card>
-    )
+            <InputField
+                label="Mot de Passe"
+                type="password"
+                placeholder="***********"
+                value={password}
+                onChange={handlePasswordChange}
+                onClick={removePlaceHolder}
+            />
+            <Error>{errors.email && errors.email}</Error>
+
+            <div className="button-container">
+                <Button onClick={goToQuizTopicsScreen}>CONNEXION</Button>
+            </div>
+
+            <div className="link-container">
+                <a href="#" className="text-white-400 hover:text-gray-300 transition">
+                    Mot de passe oublié ?
+                </a>
+                <a
+                    onClick={goToRegisterScreen} // Appel de la fonction de navigation
+                    className="text-white-400 cursor-pointer hover:text-gray-300 transition"
+                >
+                    Créer un compte
+                </a>
+            </div>
+        </Card>
+    );
 }
 
-export default Login
+export default Login;
